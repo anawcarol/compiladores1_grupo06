d3bdbf2d8bfd6795ee26fff56a0d51cf9f1343c1
IsaacLusca <isaaclucas.bezerra@gmail.com>
2025-09-26 18:19:40 -0300

    adicionando uma main para ler um arquivo lox e analisar a sintaxe

--- scr/main.c
+++ scr/main.c
@@ -1,5 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(void) {
-    return yyparse();
+int yyparse(void);
+extern FILE* yyin;
+
+int main(int argc, char** argv) {
+    if (argc < 2) {
+        fprintf(stderr, "Uso: %s arquivo.lox\n", argv[0]);
+        return 1;
+    }
+
+    yyin = fopen(argv[1], "r");
+    if (!yyin) {
+        perror("Erro ao abrir o arquivo");
+        return 1;
+    }
+
+    int ret = yyparse();
+
+    fclose(yyin);
+    return ret;
 }
