67ebba51a7088e7a6d50ea2e8697cef54aed0f0b
Pedro Ramos Sousa Reis <pedro.rsr0308@gmail.com>
2025-11-09 15:45:08 -0300

    Melhoria no código da tabela.c e tabela.h #11

--- tabela/tabela.h
+++ tabela/tabela.h
@@ -1,30 +1,23 @@
 #ifndef TABELA_H
 #define TABELA_H
 
+#include <stdlib.h>
+
+#define TAM_HASH 101
+
 typedef struct simbolo {
     char nome[32];
     char tipo[16];
+    int escopo;
     struct simbolo *prox;
 } Simbolo;
 
-
-typedef struct ambiente {
-    Simbolo *head;
-    struct ambiente *pai;
-} Ambiente;
-
 void tab_inicializar();
-
 void tab_entrarEscopo();
-
 void tab_sairEscopo();
-
 void tab_inserirSimbolo(char *nome, char *tipo);
-
 Simbolo *tab_buscarSimboloLocal(char *nome);
-
 Simbolo *tab_buscarSimbolo(char *nome);
-
 void tab_imprimirTabela();
 
 #endif
